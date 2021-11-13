4ec3048d9f7b967e6100874d4dff93913df5432a
iamshre-yash <iamshreyash.in@gmail.com>
2021-11-13 12:23:53 +0530

    pig game score 0 and hide dice

---
+++ Javascript/Pig game/script.js
@@ -0,0 +1,10 @@
+'use strict';
+//Selecting Elements
+const score0 = document.getElementById('score--0');
+const score1 = document.getElementById('score--1');
+const dice = document.getElementsByClassName('dice');
+
+//Conditions
+score0.textContent = 0;
+score1.textContent = 0;
+dice.classListlassList.add('hidden');
