8fb8dcecc7d644d06c4dd3f09878f2fd6fb51dff
iamshre-yash <iamshreyash.in@gmail.com>
2021-12-10 17:26:33 +0530

    change in reverse arr

--- C/reverseArray.c
+++ C/reverseArray.c
@@ -14,10 +14,14 @@ void main()
 {
     int arr[]={1,2,3,4,5,6,7,8,9};
     int size = sizeof(arr)/sizeof(int);
+    int arr1[size];
+    for(int i= 0; i< size; i++) {//copying arr to arr1
+      arr1[i] = arr[i];
+   }
     reverse(arr, size); //calling Function
     for (int i = 0; i < size; i++)
     {
-        printf("arr[%d] :\t %d",i, arr[i]);
+        printf("|~|arr[%d]: %d <•> arr[%d]:%d |~|",i, arr1[i],i,arr[i]);
         putchar('\n');
     }
 }
