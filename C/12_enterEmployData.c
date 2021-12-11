3244dd9e2f6a45e1173e0c4833e40e9c90fd3644
iamshre-yash <iamshreyash.in@gmail.com>
2021-12-11 18:39:38 +0530

    change all file name

---
+++ C/12_enterEmployData.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+struct employ {
+    char name[10];
+    int id;
+    char gender[1];
+};
+int main()
+{
+    int a;
+    printf("Enter Total Number of Employ : ");
+    scanf("%d",&a);
+    struct employ emp[a];
+    for (int i=0;i<a;i++)
+    {
+        printf("Enter employ %d id:  ", i+1);
+        scanf("%6d", &emp[i].id);
+        printf("Enter employ %d Gender (M / F) : ",i+1);
+        scanf("%s", emp[i].gender);
+        printf("Enter employ %d name: ",i+1);
+        scanf("%s",emp[i].name);
+        putchar('\n');
+    }
+       printf ("| • | ID     | Gender | Name \n" );
+    for(int i=0; i<a;i++)
+    {
+        printf ("| %d | %d | %s      | %s \n",i+1,emp[i].id, emp[i].gender, emp[i].name);
+    }
+ //  strcpy
+    return  0;
+}
