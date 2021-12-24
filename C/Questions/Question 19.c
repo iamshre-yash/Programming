67fc89e8430584d8c13eb3a19fc7a0ba674e58f8
iamshre-yash <https://github.com/iamshre-yash/Programming.git>
2021-12-24 18:00:10 +0530

    Question 🚀

---
+++ C/Questions/Question 19.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+typedef struct employ {
+    char name[10];
+    int id;
+    char gender[1];
+}e;
+int main()
+{
+    int a;
+    printf("Enter Total Number of Employ : ");
+    scanf("%d",&a);
+    e emp[a];
+    for (int i=0;i<a;i++)
+    {
+        printf("Enter employ %d id:  ", i+1);
+        scanf("%6d", &emp[i].id);
+        printf("Enter employ %d Gender (M / F) : ",i+1);
+        scanf("%s", emp[i].gender);
+        printf("Enter employ %d name: ",i+1);
+        scanf("%s",emp[i].name);
+        putchar('\n');
+    }
+       printf ("| • | ID     | Gender | Name \n" );
+    for(int i=0; i<a;i++)
+    {
+        printf ("| %d | %d | %s      | %s \n",i+1,emp[i].id, emp[i].gender, emp[i].name);
+    }
+    return  0;
+}
