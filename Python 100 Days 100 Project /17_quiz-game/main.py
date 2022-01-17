daf4c300c7aa30fc1ddb238c42456ee2f1fe9367
ShreYash <iamshreyash.in@gmail.com>
2022-01-17 15:13:05 +0530

    day 17 ✅

--- Python 100 Days 100 Project /17_quiz-game/main.py
+++ Python 100 Days 100 Project /17_quiz-game/main.py
@@ -5,7 +5,7 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for q in question_data:
-	question = Question(q["text"],q["answer"])
+	question = Question(q["question"],q["correct_answer"])
 	question_bank.append(question)
 quiz = QuizBrain(question_bank)
 while(quiz.still_has_questions()):
