ba77d0c98e634a4c76c7d3df336a27b99db49389
ShreYash <iamshreyash.in@gmail.com>
2022-01-19 09:15:42 +0530

    change day 19

--- Python 100 Days 100 Project /19_turtle-race_sketch/sketch.py
+++ Python 100 Days 100 Project /19_turtle-race_sketch/sketch.py
@@ -1,28 +1,28 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
+arrow = Turtle()
 screen = Screen()
 
 
 def move_forwards():
-    tim.forward(10)
+    arrow.forward(10)
 
 def move_backwards():
-    tim.backward(10)
+    arrow.backward(10)
 
 def turn_left():
-    new_heading = tim.heading() + 10
-    tim.setheading(new_heading)
+    new_heading = arrow.heading() + 10
+    arrow.setheading(new_heading)
 
 def turn_right():
-    new_heading = tim.heading() - 10
-    tim.setheading(new_heading)
+    new_heading = arrow.heading() - 10
+    arrow.setheading(new_heading)
 
 def clear():
-    tim.clear()
-    tim.penup()
-    tim.home()
-    tim.pendown()
+    arrow.clear()
+    arrow.penup()
+    arrow.home()
+    arrow.pendown()
 
 screen.listen()
 screen.onkey(move_forwards, "Up")
