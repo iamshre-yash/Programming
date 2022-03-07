51b834ae92f11385ba41e482cd3d86cc4461348a
Shre Yash <iamshreyash.in@Qgmail.com>
2022-03-07 17:42:19 +0530

    C/CPP

--- Leet Code Solution/121. Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock-1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        min_price , max_profit = float('inf'),0
-        for price in prices:
-            max_profit_sell_today = price - min_price
-            max_profit = max(max_profit, max_profit_sell_today)
-            min_price = min(min_price, price)
-        return max_profit
--- Leet Code Solution/14. Longest Common Prefix/Longest Common Prefix-1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        s1 = min(strs)
-        s2 = max(strs)
-        for i, l in enumerate(s1):
-            if l != s2[i]:
-                return s1[:i]
-        return s1
