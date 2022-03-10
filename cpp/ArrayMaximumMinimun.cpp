014950de01c08e1313e397923ebdbe65a283aeb0
Shre Yash <88998531+ishre-yash@users.noreply.github.com>
2022-03-10 19:47:59 +0530

    Add files via upload

---
+++ cpp/ArrayMaximumMinimun.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+
+void MAX(int arr[10])
+{
+	max = arr[0];
+	for(int i=0;i<10;i++)
+	{
+		if(arr[i]>max)
+		{
+			max = arr[i];
+		}
+	}
+	cout<<"MAX: "<<max;
+}
+
+oid MAX(int arr[10])
+{
+	max = arr[0];
+	for(int i=0;i<10;i++)
+	{
+		if(arr[i]>max)
+		{
+			max = arr[i];
+		}
+	}
+	cout<<"MAX: "<<max;
+}
+
+
+
+
+
+
+int main(void)
+{
+	int arr[10]={1,2,3,4,5,6,7,8,9,10};
+	displal(arr);
+	max(arr);
+	min(arr);
+}
+
